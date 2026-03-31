8b430fef3e9c68a5f10d2477dbb0db43b7fcd8db
mmije0ng <victon777@naver.com>
2026-03-31 09:55:38 +0900

    fix(member): 닉네임&팀변경 API 수정 및 DTO Response 이름 겹치는 문제 수정

--- src/main/java/com/fullcount/mapper/MemberMapper.java
+++ src/main/java/com/fullcount/mapper/MemberMapper.java
@@ -4,8 +4,30 @@ import com.fullcount.domain.Member;
 import com.fullcount.domain.MemberRole;
 import com.fullcount.domain.Team;
 import com.fullcount.dto.AuthDto;
+import com.fullcount.dto.MemberDto;
 
 public class MemberMapper {
+    // Entity -> Response DTO 변환
+    public static MemberDto.MemberResponse toMemberResponse(Member member) {
+        if (member == null) return null;
+
+        return MemberDto.MemberResponse.builder()
+                .id(member.getId())
+                .email(member.getEmail())
+                .nickname(member.getNickname())
+                .teamName(member.getTeam() != null ? member.getTeam().getName() : null)
+                .teamShortName(member.getTeam() != null ? member.getTeam().getShortName() : null)
+                .badgeLevel(member.getBadgeLevel().name())
+                .mannerTemperature(member.getMannerTemperature())
+                .role(member.getRole().name())
+                .build();
+    }
+
+    public static MemberDto.UpdateNickNameResponse toUpdateNickNameResponse(String nickName){
+        return MemberDto.UpdateNickNameResponse.builder()
+                .nickname(nickName)
+                .build();
+    }
 
     // 회원가입 시 Member 생성
     public static Member toMember(AuthDto.SignupRequest req, Team team, String encodedPassword) {
